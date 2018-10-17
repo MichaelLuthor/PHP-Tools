bad1a60404925cdf24f2e086e162bbc574d2da75
Michael Luthor <michaelluthor@163.com>
2018-10-17 21:43:20 +0800

    1

--- youtube-video-download/filedownload.php
+++ youtube-video-download/filedownload.php
@@ -1,3 +1,23 @@
 <?php
-$url = urldecode(base64_decode(base64_decode($_GET['url'])));
-readfile($url, false);
+$filepath = __DIR__.'/tmp-videos/'.uniqid();
+
+$videoFile = fopen ($filepath, 'w+');
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $_POST['url']);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_FILE, $videoFile);
+curl_exec($ch);
+curl_close($ch);
+fclose($videoFile);
+
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename=video.tmp');
+header('Content-Transfer-Encoding: binary');
+header('Expires: 0');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Pragma: public');
+header('Content-Length: ' . filesize($filepath));
+readfile($filepath);
+unlink($filepath);
