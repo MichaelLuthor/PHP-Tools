46f79f2d2b13006cb0b52be21166b2e4c50bcff2
Michael Luthor <michaelluthor@163.com>
2018-10-17 21:52:07 +0800

    .

--- youtube-video-download/filedownload.php
+++ youtube-video-download/filedownload.php
@@ -5,9 +5,14 @@ $videoFile = fopen ($filepath, 'w+');
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $_POST['url']);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 curl_setopt($ch, CURLOPT_FILE, $videoFile);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_exec($ch);
+if ( 0 !== curl_errno($ch) ) {
+    throw new \Exception("CURL 请求失败 :".curl_error($ch));
+}
 curl_close($ch);
 fclose($videoFile);
 
