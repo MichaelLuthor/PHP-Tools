a9fb32c616baaa1031616178ccc51950cc110c5d
Michael Luthor <michaelluthor@163.com>
2018-10-17 20:54:09 +0800

    .

--- youtube-video-download/filesize.php
+++ youtube-video-download/filesize.php
@@ -9,15 +9,29 @@ $response = array(
 );
 
 try {
+    $_GET['url'] = 'YUhSMGNITTZMeTl5T1MwdExYTnVMVzQ0ZGpkNmJteHlMbWR2YjJkc1pYWnBaR1Z2TG1OdmJTOTJhV1JsYjNCc1lYbGlZV05yUDI1b1BVbG5jSGRqYWtGNFRHNU9NbUo2UVRGTFoydDRUV3BqZFUxRE5IZE1ha1VsTWtOSlozQjNZMnBCZUV4dVRqSmlla0V4UzJkcmVFMXFZM1ZOUXpSM1RHcEZKbk52ZFhKalpUMTViM1YwZFdKbEpuTndZWEpoYlhNOVpIVnlKVEpEWldrbE1rTnBaQ1V5UTJsdWFYUmpkMjVrWW5CekpUSkRhWEFsTWtOcGNHSnBkSE1sTWtOcGRHRm5KVEpEYkcxMEpUSkRiV2x0WlNVeVEyMXRKVEpEYlc0bE1rTnRjeVV5UTIxMkpUSkRibWdsTWtOd2JDVXlRM0poZEdWaWVYQmhjM01sTWtOeVpYRjFhWEpsYzNOc0pUSkRjMjkxY21ObEpUSkRaWGh3YVhKbEptVjRjR2x5WlQweE5UTTVOemszTnprM0ptUjFjajB6TWprdU9UTXlKbVZwUFhoU04waFhPV0ZxUWxsaFMzbFJWelpwVEdGSlJGRW1hVzVwZEdOM2JtUmljSE05TVRFd016YzFNQ1p5WlhGMWFYSmxjM05zUFhsbGN5WnBjRDA0TUM0eU5URXVNakkxTGpJeU1TWnRjejFoZFNVeVEzSmtkU1p0ZGoxdEptMTBQVEUxTXprM056WXdOelVtY0d3OU1qUW1iV2x0WlQxMmFXUmxieVV5Um0xd05DWnBaRDF2TFVGTmExWkNRMnhuVUdKdGNuUmhRbEJwTlRCd1IwWjFkMjVEYjJSSlZYTmFORTVLYlhSV2JYWjRhR1pFSm0xdVBYTnVMVzQ0ZGpkNmJteHlKVEpEYzI0dGJqaDJOMnR1TnpZbWJXMDlNekVsTWtNeU9TWnBjR0pwZEhNOU1DWnBkR0ZuUFRJeUpteHRkRDB4TlRBNE5EZzVNVFU0TVRJMk5EVXhKbXRsZVQxNWREWW1ablpwY0QwNUpuSmhkR1ZpZVhCaGMzTTllV1Z6Sm1NOVYwVkNKbk5wWjI1aGRIVnlaVDAyUlROR01EWkNRMFEwTmpaQk5EZzROREF3TlVSR05VWXdNVEZHUlVaR05qUTVRa0U0TWpreExrRkJRa0UyTVVVNU9UWkJRa0kxTXpneE1EazBNamd4UmpJMFJFRkVSakV3TWpCRk16WkVNa01tZG1sa1pXOWZhV1E5UlRWV2FIRmFiMUJOUm1NbWRHbDBiR1U5TVRVdE1UWXJURWxDVWtVckpVVXpKVGd5SlVJNUpVVXpKVGd6SlRoRkpVVXpKVGd6SlVKREpVVXpKVGd6SlRsREpVVXpKVGd6SlVKREpVVXpKVGd6SlRnNUt5VkZNeVU0TWlWQ01DVkZNeVU0TXlWQk9TVkZNeVU0TXlVNE9DVkZNeVU0TXlWQlFTc2xSVE1sT0RJbFFVUWxSVE1sT0RNbE9ETWxSVE1sT0RJbFFVSWxSVE1sT0RNbFFrTT0=';
+    
     $url = urldecode(base64_decode(base64_decode($_GET['url'])));
     
+    $url = 'https://r9---sn-n8v7znlr.googlevideo.com/videoplayback?nh=IgpwcjAxLnN2bzA1KgkxMjcuMC4wLjE%2CIgpwcjAxLnN2bzA1KgkxMjcuMC4wLjE&source=youtube&sparams=dur%2Cei%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cnh%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cexpire&expire=1539797797&dur=329.932&ei=xR7HW9ajBYaKyQW6iLaIDQ&initcwndbps=1103750&requiressl=yes&ip=80.251.225.221&ms=au%2Crdu&mv=m&mt=1539776075&pl=24&mime=video%2Fmp4&id=o-AMkVBClgPbmrtaBPi50pGFuwnCodIUsZ4NJmtVmvxhfD&mn=sn-n8v7znlr%2Csn-n8v7kn76&mm=31%2C29&ipbits=0&itag=22&lmt=1508489158126451&key=yt6&fvip=9&ratebypass=yes&c=WEB&signature=6E3F06BCD466A4884005DF5F011FEFF649BA8291.AABA61E996ABB5381094281F24DADF1020E36D2C&video_id=E5VhqZoPMFc&title=15-16+LIBRE+%E3%82%B9%E3%83%8E%E3%83%BC%E3%83%9C%E3%83%BC%E3%83%89+%E3%82%B0%E3%83%A9%E3%83%88%E3%83%AA+%E3%82%AD%E3%83%83%E3%82%AB%E3%83%BC';
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_HEADER, 1);
     curl_setopt($ch, CURLOPT_NOBODY, 1);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    
+    curl_setopt($ch, CURLOPT_PROXY, '127.0.0.1');
+    curl_setopt($ch, CURLOPT_PROXYPORT, 1080);
+    curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+    
     $sizeResponse = curl_exec($ch);
+    
+    if ( 0 !== curl_errno($ch) ) {
+        throw new \Exception("CURL 请求失败 :".curl_error($ch));
+    }
+    
     if ( false === $sizeResponse ) {
         throw new \Exception("CURL 请求失败 :".curl_error($ch));
     }
